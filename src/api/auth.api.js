a4d0cbaf1d76ddc2b6fa7843df22269093a22470
namnguyen <ngnclht@gmail.com>
2020-08-22 10:00:52 +0700

    Update api

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,7 +1,7 @@
 import { unauthenticatedPost } from './helper'
 
 export const login = async ({ email, password}) =>  {
-  return await unauthenticatedPost('/public/login', {
+  return await unauthenticatedPost('/admin/public/login', {
     email, password
   }, true)
 }
