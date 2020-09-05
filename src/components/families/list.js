ebe6b3c3131edacb053e062518134f6e18c85da6
namnguyen <ngnclht@gmail.com>
2020-09-05 09:16:42 +0700

    WIP

--- src/components/families/list.js
+++ src/components/families/list.js
@@ -1,8 +1,66 @@
 import React from "react";
 import { List, Datagrid, TextField } from 'react-admin';
+import { cloneElement } from 'react';
+import {
+    useListContext,
+    TopToolbar,
+    CreateButton,
+    ExportButton,
+    Button,
+    sanitizeListRestProps,
+} from 'react-admin';
+import IconEvent from '@material-ui/icons/Event';
+
+const ListActions = (props) => {
+    const {
+        className,
+        exporter,
+        filters,
+        maxResults,
+        ...rest
+    } = props;
+    const {
+        currentSort,
+        resource,
+        displayedFilters,
+        filterValues,
+        hasCreate,
+        basePath,
+        selectedIds,
+        showFilter,
+        total,
+    } = useListContext();
+    return (
+        <TopToolbar className={className} {...sanitizeListRestProps(rest)}>
+            {filters && cloneElement(filters, {
+                resource,
+                showFilter,
+                displayedFilters,
+                filterValues,
+                context: 'button',
+            })}
+            <CreateButton basePath={basePath} />
+            <ExportButton
+                disabled={total === 0}
+                resource={resource}
+                sort={currentSort}
+                filterValues={filterValues}
+                maxResults={maxResults}
+            />
+            {/* Add your custom actions */}
+            <Button
+                onClick={() => { alert('Your custom action'); }}
+                label="Show calendar"
+            >
+                <IconEvent />
+            </Button>
+        </TopToolbar>
+    );
+};
+
 
 const FamilyList = props => (
-  <List {...props}>
+  <List {...props} title="Danh sách dòng họ" actions={<ListActions />}>
       <Datagrid rowClick="edit">
           <TextField source="id" />
           <TextField source="name" />
